37032c64025b0791e07d7baec01e5ebb24d5a269
karaevelize <k.evelize@gmail.com>
2019-12-16 11:32:05 +0100

    Login and Sign-Up are working

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRouter = require("./auth/router");
+const userRouter = require("./user/router");
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+const corsMiddleware = cors();
+const parserMiddleware = bodyParser.json();
+
+app.use(corsMiddleware);
+app.use(parserMiddleware);
+
+app.use(authRouter);
+app.use(userRouter);
+
+app.listen(port, () => console.log(`Hey, I'm on port ${port}!`));
---
+++ user/router.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const User = require("./model");
+const router = new Router();
+const bcrypt = require("bcrypt");
+
+router.post("/user", (request, response, next) => {
+  const user = {
+    email: request.body.email,
+    password: bcrypt.hashSync(request.body.password, 3)
+  };
+  User.create(user)
+    .then(user => response.send(user))
+    .catch(errors => next(errors));
+});
+
+router.get("/user", (request, response, next) => {
+  const limit = request.query.limit || 10;
+  const offset = request.query.offset || 0;
+
+  User.findAndCountAll({ limit, offset })
+    .then(result => response.send({ events: result.rows, total: result.count }))
+    .catch(error => next(error));
+});
+
+module.exports = router;
