1b4116a9fd3b31301e8dd9b75dff33160635e5ac
karaevelize <k.evelize@gmail.com>
2019-12-18 17:01:24 +0100

    Added a bew router

--- game-room/model.js
+++ game-room/model.js
@@ -1,8 +1,19 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const User = require("../user/model");
 
 const Room = db.define("room", {
-  name: Sequelize.STRING
+  name: Sequelize.STRING,
+  players: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 2,
+      max: 2
+    }
+  }
 });
+
+User.belongsTo(Room);
+Room.hasMany(User);
 
 module.exports = Room;
--- game-room/router.js
+++ game-room/router.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Room = require("./model");
+const User = require("../user/model");
 
 function factory(stream) {
   const router = new Router();
@@ -15,6 +16,19 @@ function factory(stream) {
 
       stream.send(string);
       response.send(room);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put("/join", async (request, response, next) => {
+    try {
+      const user = await User.update(
+        { roomId: request.body.roomId },
+        { where: { id: request.body.userId } }
+      );
+
+      response.send(user);
     } catch (error) {
       next(error);
     }
